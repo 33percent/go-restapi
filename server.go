bccde8411da0d8bef6796b16fb219aad3a78300b
sandeep <sandeep0814@gmail.com>
2018-05-07 23:09:31 +0530

    first commit

--- server.go
+++ server.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-  "net/http"
-  "strings"
-  _ "github.com/go-sql-driver/mysql"
   "fmt"
 )
 
 
 
 func main() {
-  if err := http.ListenAndServe(":3000", nil); err != nil {
-    panic(err)
-  }
-  http.HandleFunc("/create", createtodo)
-  http.HandleFunc("/update",updatetodo)
-  http.HandleFunc("/delete",deletetodo)
-  http.HandleFunc("/read",readtodo)
-
+fmt.Println("aksdas")
 }
